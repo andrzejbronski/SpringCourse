cc58b3e86ddffb40f822ab2e79bd35d004dae9de
Andrzej Broński <andbronski@gmail.com>
2019-03-14 00:07:09 +0100

    Spring components and automated relations

--- SpringFramework-2/src/main/java/app/SpringDiApplication.java
+++ SpringFramework-2/src/main/java/app/SpringDiApplication.java
@@ -3,12 +3,16 @@ package app;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import beans.MessagePrinter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan(basePackageClasses = MessagePrinter.class)
 public class SpringDiApplication {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx =
-                new AnnotationConfigApplicationContext(ApplicationConfig.class);
+                new AnnotationConfigApplicationContext(SpringDiApplication.class);
         MessagePrinter printer = ctx.getBean(MessagePrinter.class);
         printer.print();
         ctx.close();
--- SpringFramework-2/src/main/java/beans/MessagePrinter.java
+++ SpringFramework-2/src/main/java/beans/MessagePrinter.java
@@ -1,7 +1,11 @@
 package beans;
 
-public class MessagePrinter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class MessagePrinter {
+@Autowired
     private MessageProducer producer;
 
     MessagePrinter() {}
