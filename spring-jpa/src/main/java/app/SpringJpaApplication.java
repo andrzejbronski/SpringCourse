1233f60ebd365d194ca0abd619f44ec0e3d63daf
Andrzej Broński <andbronski@gmail.com>
2019-03-19 08:36:53 +0100

    JPA & Spring - MySQL

---
+++ spring-jpa/src/main/java/app/SpringJpaApplication.java
@@ -0,0 +1,23 @@
+package app;
+
+import org.springframework.context.annotation.*;
+import app.dao.BookDao;
+import app.model.Book;
+
+@Configuration
+@ComponentScan
+public class SpringJpaApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx =
+                new AnnotationConfigApplicationContext(SpringJpaApplication.class);
+        BookDao bookDao = ctx.getBean(BookDao.class);
+        //zapisujemy
+        Book book = new Book("1234567890468", "Spring is so cool", "Andrzej");
+        bookDao.save(book);
+        //odczytujemy
+        Book bookGet = bookDao.get(1L);
+        System.out.println(bookGet);
+        ctx.close();
+    }
+}
